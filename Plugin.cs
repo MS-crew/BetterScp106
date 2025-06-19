6912130a8376de13eed6cc7eba75a996a7630da2
Mustafa SAVAŞ <mustafavesavas1@gmail.com>
2025-06-19 23:43:03 +0300

    Deleted wrong harmony id

--- Plugin.cs
+++ Plugin.cs
@@ -96,7 +96,7 @@ namespace BetterScp106
             PlayerHandlers.EscapingPocketDimension -= EventHandlers.OnEscapingPocketDim;
             PlayerHandlers.FailingEscapePocketDimension -= EventHandlers.OnFailingEscape;
 
-            this.harmony.UnpatchAll(harmonyID: "Better106Patchs");
+            this.harmony.UnpatchAll();
             EventHandlers = null;
             Instance = null;
             base.OnDisabled();
